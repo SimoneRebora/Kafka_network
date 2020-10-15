fc1ff2962b50e287e312d4c01c294d4985e51314
SimoneRebora <simone.rebora81@gmail.com>
2020-10-15 11:57:52 +0200

    undirected networks

--- build_LIWC_network.R
+++ build_LIWC_network.R
@@ -3,18 +3,22 @@ library(lsa)
 # upload LIWC table
 LIWC_analysis <- read.csv("LIWC_analysis.csv", row.names = 1, stringsAsFactors = F)
 
+# to do: exclude short texts
+LIWC_analysis$WC[which(LIWC_analysis$WC < 5000)] # let's decide on threshold (5,000 or 2,000?)
+
 # remove first two columns
 LIWC_analysis <- LIWC_analysis[,-c(1,2)]
 
 # reduce analysis to just a few selected columns
 my_selection <- 1:96
 LIWC_analysis <- LIWC_analysis[,my_selection]
+# try also the other dimensions?
 
 # calculate distance matrix using cosine
 distance_matrix <- cosine(t(as.matrix(LIWC_analysis)))
 
 # set number of nearest neighbours
-nearest_neighbours <- 10
+nearest_neighbours <- 5
 
 # loop on the distance matrix to create list of edges
 edges_df <- data.frame()
@@ -31,6 +35,8 @@ for(i in 1:dim(distance_matrix)[1]){
   
 }
 
+edges_df$type <- "undirected" # or "directed"
+
 # write list of edges
 write.csv(edges_df, file = paste("edges_", nearest_neighbours, "neighbours.csv", sep = ""), row.names = F)
 
@@ -38,6 +44,16 @@ write.csv(edges_df, file = paste("edges_", nearest_neighbours, "neighbours.csv",
 nodes_list <- unique(c(edges_df$source, edges_df$target))
 groups <- strsplit(nodes_list, "_")
 groups <- unlist(lapply(groups, function(x) x[1]))
+
+# add another group (decades)
+nodes_list <- unique(c(edges_df$source, edges_df$target))
+years <- strsplit(nodes_list, "_")
+years <- unlist(lapply(years, function(x) x[3])) # perhaps 4 instead of 3
+years <- gsub(pattern = ".txt", replacement = "", years)
+# to do: divide into decades
+
+# another one: nationality
+# to be added manually in third (?) position in the titles 
 
 nodes_df <- data.frame(ID = nodes_list, label = nodes_list, groups, stringsAsFactors = F)
 
